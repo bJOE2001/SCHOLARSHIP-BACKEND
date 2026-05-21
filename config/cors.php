5473ef869df5efacbdb8356d6c875c751a0d9970
BELLY JOE BUENO BASADRE <132354249+bJOE2001@users.noreply.github.com>
2026-05-21 17:57:15 +0800

    feat: update CORS configuration to use environment variable for allowed origins

--- config/cors.php
+++ config/cors.php
@@ -6,8 +6,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:9000',
-        'http://127.0.0.1:9000',
+        ...array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:9000,http://127.0.0.1:9000')))),
     ],
 
     'allowed_headers' => ['*'],
